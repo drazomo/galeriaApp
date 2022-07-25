3a313dbb851c18ed4858cdad8e778b78d6de023b
Mark Rasavong <rasavong.mark@gmail.com>
2022-07-25 17:14:02 +0200

    Style Navbar with mobile responsiveness

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,29 +1,40 @@
 import React from 'react';
 import { Link } from '../GeneralComponents/General.styled';
-import { NavBarContainer, NavIcnContainer, NavIconBtn, NavSearchBar } from './Navbar.styled';
+import { NavBarContainer, NavIcnContainer, NavIconBtn, NavIconItem, NavIconTitle, NavSearchBar } from './Navbar.styled';
 import {ReactComponent as CameraIcon} from './menu_cameraIcon.svg';
 import {ReactComponent as HeartIcon} from './menu_heartIcon.svg';
 import {ReactComponent as ThemeIcon} from './menu_themeIcon.svg';
+import {ReactComponent as SearchIcon} from './menu_searchIcon.svg';
 
 
 
 const iconButtons = [
-  {sauce: <CameraIcon />, alt: 'Camera Icon'},
-  {sauce: <HeartIcon />, alt: 'Heart Icon'},
-  {sauce: <ThemeIcon />, alt: 'Theme Icon'},
+  {sauce: <CameraIcon />, alt: 'Camera Icon', name: 'Photos', className: 'cls__icn_btn_photos'},
+  {sauce: <HeartIcon />, alt: 'Heart Icon', name: 'Saved', className: 'cls__icn_btn_heart'},
+  {sauce: <ThemeIcon />, alt: 'Theme Icon', name: 'Theme', className: 'cls__icn_btn_theme'},
 ]
 
 const Navbar = () => {
   return (
     <NavBarContainer>
-      <NavSearchBar type={'text'}/>
+      <div className="searchBarContainer">
+        <NavSearchBar type={'text'} placeholder='Search...'/>
+        <div className='searchIcnContainer'>
+          <SearchIcon className='searchIcn'/>
+        </div>
+      </div>
       <NavIcnContainer>
-        {iconButtons.map(({sauce, alt}) => (
-            <NavIconBtn key={`icnBtn_${alt.replace(/\w/g, '')}`}>
+        {iconButtons.map(({sauce, alt, name, className}) => (
+          <NavIconItem>
+            <NavIconBtn key={`icnBtn_${alt.replace(/\w/g, '')}`} className={className}>
               <Link>
-                {sauce}
+                <div style={{ width: '28px', height: '25px'}}>
+                  {sauce}
+                </div>
               </Link>
             </NavIconBtn>
+            <NavIconTitle>{name}</NavIconTitle>
+          </NavIconItem>
         ))
         }
       </NavIcnContainer>
