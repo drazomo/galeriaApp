b4354284c3881e395f54eb35fc1266f275878019
Mark Rasavong <rasavong.mark@gmail.com>
2022-08-01 18:58:06 +0200

    Style convert grid to occupy verticle space

--- src/components/Mosaic/Mosaic.styled.ts
+++ src/components/Mosaic/Mosaic.styled.ts
@@ -1,22 +1,21 @@
 import styled from 'styled-components';
 
 export const MosaicGrid = styled.div`
-  display: grid;
-  grid-template-columns: repeat(3, 279.33px);
-  grid-gap: 1em;
+  columns: 3 100px;
+  column-gap: 1em;
 
   margin-top: 1em;
   padding: 0;
   background-color: gray;
   width: 864px;
-  height: 100%;
 `;
 
 export const MosaicBody = styled.div`
   img {
     border-radius: 16px;
     width: 100%;
-    height: 100%;
+    display: block;
+    margin-bottom: 1em;
   }
 `;
 
