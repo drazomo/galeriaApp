9d8051d02c8983ecc724f1c462b9a8a42a675b4e
Mark Rasavong <rasavong.mark@gmail.com>
2022-09-15 14:10:34 +0200

    add: picModal to mosaic shape images

--- src/components/Mosaic/MosaicTile.tsx
+++ src/components/Mosaic/MosaicTile.tsx
@@ -1,16 +1,37 @@
+import { useState } from 'react';
+import { UnsplashDataProps } from '../../features/feed';
+import PicModal from '../PicModal/PicModal';
 import { MosaicBody } from './Mosaic.styled';
 
 interface MosaicTileProps {
   image: string;
   alt?: string;
+  item: UnsplashDataProps
 }
 
 
-const MosaicTile= ({ image, alt }: MosaicTileProps) => {
+const MosaicTile= ({ image, alt, item }: MosaicTileProps) => {
+  const [modalOpen, setModalOpen] = useState(false)
+
+  const showModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  const handleOnClick = () => {
+    showModal();
+  }
+
   return (
-    <MosaicBody >
-      <img src={image} alt={alt} />
-    </MosaicBody>
+    <>
+      <PicModal key={item.id} item={item} onClose={closeModal} open={modalOpen}/>
+      <MosaicBody onClick={handleOnClick}>
+        <img src={image} alt={alt} />
+      </MosaicBody>
+    </>
   )
 };
 
