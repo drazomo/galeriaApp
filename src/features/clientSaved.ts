ca7afc150332d089d3c9c67eafd6ca86625c9403
Mark Rasavong <rasavong.mark@gmail.com>
2022-09-24 15:16:09 +0200

    add: clientSaved slice for rendering photos and collections

--- src/features/clientSaved.ts
+++ src/features/clientSaved.ts
@@ -1,31 +1,52 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { COLLECTIONS_IDS } from "../utils";
+import { SAVED_COLLECTIONS, SAVED_IMGS } from "../utils";
 import { UnsplashDataProps } from "./feed";
 
 const initialState = {
   selectedCollections: {},
+  selectedPhotos: {},
 }
 
 const clientSaved = createSlice({
   name: 'clientSaved',
   reducers: {
     fetchSavedCollections(state){
-      state.selectedCollections = JSON.parse(localStorage.getItem(COLLECTIONS_IDS) as string) || {}
+      state.selectedCollections = JSON.parse(localStorage.getItem(SAVED_COLLECTIONS) as string) || {}
+      state.selectedCollections = JSON.parse(localStorage.getItem(SAVED_IMGS) as string) || {}
     },
     saveCollection(state, action) {
       let savedCollections:Record<string, UnsplashDataProps> = {}
-      if(localStorage.getItem(COLLECTIONS_IDS) !== null) {
-        savedCollections = JSON.parse(localStorage.getItem(COLLECTIONS_IDS) as string)
+      //FOR COLLECTIONS
+      if(action.payload.type === 'collection') {
+        if(localStorage.getItem(SAVED_COLLECTIONS) !== null) {
+          savedCollections = JSON.parse(localStorage.getItem(SAVED_COLLECTIONS) as string)
+        }
+        savedCollections[action.payload.id] = action.payload
+        localStorage.setItem(SAVED_COLLECTIONS, JSON.stringify(savedCollections))
+        state.selectedCollections = savedCollections
+      } else {
+        //FOR PHOTOS
+        if(localStorage.getItem(SAVED_IMGS) !== null) {
+          savedCollections = JSON.parse(localStorage.getItem(SAVED_IMGS) as string)
+        }
+        savedCollections[action.payload.id] = action.payload
+        localStorage.setItem(SAVED_IMGS, JSON.stringify(savedCollections))
+        state.selectedPhotos = savedCollections
       }
-      savedCollections[action.payload.id] = action.payload
-      localStorage.setItem(COLLECTIONS_IDS, JSON.stringify(savedCollections))
-      state.selectedCollections = savedCollections
     },
     removeCollection(state, action) {
-      let savedCollections = JSON.parse(localStorage.getItem(COLLECTIONS_IDS) as string)
-      delete savedCollections[`${action.payload}`]
-      localStorage.setItem(COLLECTIONS_IDS, JSON.stringify(savedCollections))
-      state.selectedCollections = savedCollections
+      //FOR COLLECTIONS
+      if(action.payload.type === 'collection') {
+        let savedCollections = JSON.parse(localStorage.getItem(SAVED_COLLECTIONS) as string)
+        delete savedCollections[`${action.payload.id}`]
+        localStorage.setItem(SAVED_COLLECTIONS, JSON.stringify(savedCollections))
+        state.selectedCollections = savedCollections
+      } else {
+        let savedCollections = JSON.parse(localStorage.getItem(SAVED_IMGS) as string)
+        delete savedCollections[`${action.payload.id}`]
+        localStorage.setItem(SAVED_IMGS, JSON.stringify(savedCollections))
+        state.selectedPhotos = savedCollections
+      }
     }},
   initialState
 })
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,10 +1,17 @@
-export const COLLECTIONS_IDS = 'collectionsid'
+export const SAVED_COLLECTIONS = 'savedCollections'
+export const SAVED_IMGS = 'savedImgs'
 
-export const collectionExistInLocalStorage = (id: string) => {
-  const storedItems = localStorage.getItem(COLLECTIONS_IDS);
+interface ColExistInLocalStorageProps {
+  id: string
+  savedType: typeof SAVED_COLLECTIONS | typeof SAVED_IMGS
+}
+
+export const collectionExistInLocalStorage = ({id, savedType}: ColExistInLocalStorageProps) => {
+  const storedItems = localStorage.getItem(savedType);
   if (!storedItems) {
     return false;
   }
+
   const topicStore = JSON.parse(storedItems);
   return topicStore.hasOwnProperty(id);
 }
