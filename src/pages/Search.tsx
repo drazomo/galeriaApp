8499943f9a1e4af9551354a978bcaa3dcef41749
Mark Rasavong <rasavong.mark@gmail.com>
2022-09-27 18:28:12 +0200

    add: render search query photos

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,9 +1,13 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { useAppDispatch, useAppSelector } from '../app/hooks'
+import ExploreImage from '../components/ExploreImage'
 import { Container } from '../components/ExploreImage/ExploreImage.styled'
 import FilterHeader from '../components/FilterHeader'
 import { LinkBtn, LinkItem } from '../components/FilterHeader/FilterHeader.styled'
 import { Grid } from '../components/LrgCollectionCard/LrgCollectionCard.styled'
+import { UnsplashDataProps } from '../features/feed'
+import { queryCollectionsSearch, queryPhotosSearch } from '../features/searchResults'
 
 interface ParamsInterface {
   query: string
@@ -12,8 +16,25 @@ interface ParamsInterface {
 const filterOptions = ['Photos', 'Collections']
 
 const Search = () => {
+  const dispatch = useAppDispatch()
+  const { collectionPage, photoPage, photoResults, collectionResults } = useAppSelector(state => state.searchResults)
   const [checked, setChecked] = useState('photos')
   const { query } = useParams<keyof ParamsInterface>() as ParamsInterface
+
+  useEffect(() => {
+    if(query !== '') {
+      dispatch(queryCollectionsSearch({query, page: collectionPage }))
+      dispatch(queryPhotosSearch({query, page: photoPage }))
+    }
+  }, [])
+
+  const photos = (photoResults.results).map(foto => (
+    <ExploreImage
+      key={`${foto.id}_gridSavedCollection`}
+      item={foto}
+      grid
+    />
+  ))
 
   
   const handleLinkBtnClick = (value: string) => {
@@ -45,7 +66,7 @@ const Search = () => {
     <Container>
       <Grid>
         {
-          checked === 'photos' ? 'photos' : 'collections'
+          checked === 'photos' ? photos : 'collections'
         }
       </Grid>
     </Container>
