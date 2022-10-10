10b13974f3c06b7e9e00026e1a08a1c4fbfdf946
Mark Rasavong <rasavong.mark@gmail.com>
2022-10-10 18:55:16 +0200

    Add: setup routing for 404 page

--- src/components/CollectionDetails/CollectionDetails.styled.ts
+++ src/components/CollectionDetails/CollectionDetails.styled.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import {theme} from '../../styles/theme'
+import { theme } from '../../styles/theme'
 
 export const CollectionDetailsContainer = styled.div`
 display: flex;
