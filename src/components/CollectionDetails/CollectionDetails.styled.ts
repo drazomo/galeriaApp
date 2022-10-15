5d2b8daac07b071b7c3b8fa90753ad12c9586585
Mark Rasavong <rasavong.mark@gmail.com>
2022-10-15 15:57:26 +0200

    Style: CollectionDetails mobile responsivesness

--- src/components/CollectionDetails/CollectionDetails.styled.ts
+++ src/components/CollectionDetails/CollectionDetails.styled.ts
@@ -1,10 +1,26 @@
 import styled from "styled-components";
-import { theme } from '../../styles/theme'
+import { size, theme } from '../../styles/theme'
 
 export const CollectionDetailsContainer = styled.div`
 display: flex;
 align-items: center;
 width: 870px;
+
+@media (max-width: ${size.tablet}){
+  width: 700px;
+}
+
+@media (max-width: ${size.mobileL}){
+  width: 400px;
+}
+
+@media (max-width: ${size.mobileM}){
+  width: 340px;
+}
+
+@media (max-width: ${size.mobileS}){
+  width: 310px;
+}
 `;
 
 export const CollectionImg = styled.img`
@@ -27,6 +43,26 @@ h2 {
 p {
   margin-top: 0.7em;
   font-weight: 600;
+}
+
+@media (max-width: ${size.mobileL}){
+  p {
+    font-weight: 400;
+  }
+}
+
+@media (max-width: ${size.mobileM}){
+margin-left: 2em;
+  p {
+    display: none;
+  }
+}
+
+@media (max-width: ${size.mobileS}){
+margin-left: 1em;
+  p {
+    display: none;
+  }
 }
 `;
 
